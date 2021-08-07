f1c2cd40e4ce9fd8b3c957b6ad0d0342bb09cc50
ihsenkilani <58074460+ihsenkilani@users.noreply.github.com>
2021-08-07 21:52:38 +0100

    07/08/2021

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,4 @@
+<div class="panel panel-default" style="background-color: rgba(16, 80, 177, 0.76)">
+    <div class="panel-body" style="color: floralwhite"> Panel Content</div>
+    <div class="panel-footer" >Panel Footer</div>
+  </div>
